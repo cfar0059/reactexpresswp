7ea8330e0ac589e55add49e5f037ed3f6cd47b23
cfar0059 <cfar0059@gmail.com>
2020-10-09 17:33:05 +0200

    Navbar - Removed Icon from props.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navbar = ({icon, title}) => {
+const Navbar = ({title}) => {
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="container">
