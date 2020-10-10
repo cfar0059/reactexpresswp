8aea0724791907ae8f4ca31fbafa6736ca387c9c
cfar0059 <cfar0059@gmail.com>
2020-10-10 14:25:04 +0200

    Installation and set up of dotenv and nodemailer.

--- server/src/routes/sendForm.js
+++ server/src/routes/sendForm.js
@@ -1,9 +1,37 @@
 const express = require('express')
 const sendFormRouter = express.Router()
+const transporter = require('./config');
+const dotenv = require('dotenv');
+dotenv.config();
 
 sendFormRouter.post('/api/send', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+    try {
+        const mailOptions = {
+            from: req.body.email,
+            to: process.env.email,
+            subject: req.body.subject,
+            html: req.body.message
+        };
+
+        transporter.sendMail(mailOptions, function(err, info) {
+            if (err) {
+                res.status(500).send({
+                    success: false,
+                    message: 'Something went wrong. Try again later'
+                });
+            } else {
+                res.send({
+                    success: true,
+                    message: 'Thanks for contacting us. We will get back to you shortly'
+                });
+            }
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: 'Something went wrong. Try again later'
+        });
+    }
+});
 
 module.exports = sendFormRouter
