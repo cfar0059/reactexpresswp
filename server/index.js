2530cf33fd722889a0257a95ade836bb8ab3cb20
cfar0059 <cfar0059@gmail.com>
2020-10-11 12:52:37 +0200

    Changes for heroku

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const importformRouter = require('./src/routes/importForm')
 const sendFormRouter = require('./src/routes/sendForm')
 app.use('/', [importformRouter, sendFormRouter]);
 
-const PORT = process.env.PORT || 3030
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log('server start on port 3030');
+    console.log(`server started on port ${PORT}`);
 });
